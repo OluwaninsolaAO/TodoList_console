9ec65d6e86bbddd7417b9dbf99801917223950f8
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-23 18:50:16 +0100

    Implement BaseModel to handle creation of new instances through kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -12,6 +12,25 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Constructor method for the class BaseModel"""
         if not kwargs:
-            self.id = uuid4()
+            self.id = str(uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
+        else:
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+
+    def __str__(self):
+        """string representation"""
+        return "[{}] <{}> {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
+    def to_dict(self):
+        """Returns a dictionary representation of the class
+        instance using the format below:
+        {<cls>.<id>:{<cls>, <dict>}"""
+        obj = {}
+        obj.update(self.__dict__)
+        obj.update({'__class__':self.__class__.__name__})
+        key = self.__class__.__name__ + '.' + self.id
+        return {key: obj}
+
---
+++ models/todo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""models/todo.py"""
+from models.base_model import BaseModel
+
+class Todo(BaseModel):
+    """This defines each todo items in our application"""
+    pass
